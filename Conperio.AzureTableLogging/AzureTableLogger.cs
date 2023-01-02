0110f00e2f53e595e1ca09d2299339d6fe2551cd
Tor Jonsson <tor.jonsson@conperio.se>
2023-01-02 17:49:31 +0100

    Defer tableClient.CreateIfNotExists to first enabled call to Log

--- Conperio.AzureTableLogging/AzureTableLogger.cs
+++ Conperio.AzureTableLogging/AzureTableLogger.cs
@@ -10,6 +10,7 @@ public partial class AzureTableLogger : ILogger
     private readonly TableClient _tableClient;
     private readonly string _name;
     private readonly Func<AzureTableLoggerConfiguration> _getCurrentConfig;
+    private bool _initialized = false;
 
 
     public AzureTableLogger(string name, Func<AzureTableLoggerConfiguration> config)
@@ -18,14 +19,19 @@ public partial class AzureTableLogger : ILogger
         _getCurrentConfig = config;
         
         _tableClient = new TableClient(_getCurrentConfig().ConnectionString, _getCurrentConfig().Table);
-        _tableClient.CreateIfNotExists();
     }
 
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
 
     public bool IsEnabled(LogLevel logLevel)
     {
-        return _getCurrentConfig().LogLevel <= logLevel;
+        return _getCurrentConfig().LogLevel <= logLevel && _initialized;
+    }
+
+    private void Initialize()
+    {
+        _tableClient.CreateIfNotExists();
+        _initialized = true;
     }
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
@@ -35,19 +41,24 @@ public partial class AzureTableLogger : ILogger
             return;
         }
 
+        if (!_initialized)
+        {
+            Initialize();
+        }
+
         var log = new TableLogEntry
         {
             PartitionKey = DateTime.Now.ToString("yyyyMMdd"),
             RowKey = Guid.NewGuid().ToString(),
             EventId = eventId.Id,
             LogLevel = (int)logLevel,
-            Message = MessageFormatter(state, exception, formatter)
+            Message = FormatMessage(state, exception, formatter)
         };
 
         _tableClient.AddEntity(log);
     }
 
-    private static string MessageFormatter<TState>(TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    private static string FormatMessage<TState>(TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
         // The default formatter only formats the state.
         var sb = new StringBuilder();
---
+++ LoggingApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using Conperio.AzureTableLogging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var azureStorageConnectionString = Environment.GetEnvironmentVariable("AzureStorageConnectionString");
+
+if(string.IsNullOrEmpty(azureStorageConnectionString))
+{
+    throw new Exception("AzureStorageConnectionString environment variable not set");
+}
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(builder =>
+        builder.ClearProviders()
+
+            .AddAzureTableLogger(configuration =>
+            {
+                configuration.ConnectionString = azureStorageConnectionString;
+                configuration.Table = "TestLogs";
+                configuration.LogLevel = LogLevel.Information;
+            }))
+            .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+
+logger.LogDebug(1, "Does this line get hit?");    // Not logged
+logger.LogInformation(7, "Some information. {Date}", DateTime.Now);  // Logged with parameters
+logger.LogError(7, "Oops, there was an error.");  // Logged
+try
+{
+    var t1 = 1 - 1;
+    var t = 100 / t1;
+}
+catch(Exception e)
+{
+    logger.LogError(e, "An error occured"); // Logged with stacktrace and exception message
+}
+
+
+
+await host.RunAsync();
